35f9d925e651f12af29ea9318c8fbdd68411dfde
Ignat Harczuk <ignathe@gmail.com>
2015-03-04 15:52:14 +0100

    initiate builder, add gitignore

---
+++ src/builder.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import molecules
+
+class Builder( object ):
+    def __init__(self):
+        """docstring for __init__"""
+
+def build_SO4H2( r_so = ):
+    """Return molecule with sulfur in origo, one hydrogen in xz plane"""
+        
+
+if __name__ == '__main__':
+    main()
+
