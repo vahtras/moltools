382acfb351a9744f67e063567baaec2ca07a49d7
Ignat Harczuk <ignathe@gmail.com>
2015-03-05 13:57:12 +0100

    added optimized pna_opt to builder at 6-31+G* /b3lyp

--- src/builder.py
+++ src/builder.py
@@ -7,12 +7,32 @@ import molecules, os
 def sulfuric_acid():
     """Return geo opt. 
     molecule with sulfur in origo, one oxygen in xz plane"""
-    FILE = os.path.join( os.path.dirname( os.path.realpath( __file__) ) , "build/sulfur_opt.xyz" )
+    builddir = "build"
+    molfile = "sulfur_opt.xyz"
+    FILE = os.path.join( os.path.dirname( os.path.realpath( __file__) ) , os.path.join( builddir, molfile ))
     m = molecules.Molecule.from_xyz( FILE )
     return m
 
-        
+
+def paranitro_aniline():
+    """Return geo opt. 
+    molecule with sulfur in origo, one oxygen in xz plane"""
+    builddir = "build"
+    molfile = "pna_opt.xyz"
+    FILE = os.path.join( os.path.dirname( os.path.realpath( __file__) ) , os.path.join( builddir, molfile ))
+    m = molecules.Molecule.from_xyz( FILE )
+    return m
+
+def tip3p():
+    """Return geo opt. 
+    molecule with sulfur in origo, one oxygen in xz plane"""
+    builddir = "build"
+    molfile = "tip3p.xyz"
+    FILE = os.path.join( os.path.dirname( os.path.realpath( __file__) ) , os.path.join( builddir, molfile ))
+    m = molecules.Water.get_standard( AA = True )
+
+    return m
+
 
 if __name__ == '__main__':
     main()
-
