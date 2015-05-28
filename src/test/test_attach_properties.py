52c3ccedd8a18ee7f55d7023f69bdcf68cb19d03
Ignat Harczuk <ignathe@gmail.se>
2015-05-28 12:45:56 +0200

    new euler with lambdas working

---
+++ src/test/test_attach_properties.py
@@ -0,0 +1,54 @@
+import unittest, os
+import numpy as np
+
+import utilz
+from molecules import Molecule, Property, Water
+
+WATER = """3
+
+OW               22.96300  30.31700  45.73800
+HW1              23.12300  30.72100  46.59000
+HW2              22.51700  30.99500  45.23000"""
+
+class AttachPropertyTest( unittest.TestCase ):
+
+    def setUp(self):
+        pass
+
+    def test_attach_properties(self):
+        w = Water.get_standard()
+        t, r1, r2, r3 = utilz.center_and_xz( w.o.r, (w.h2.r - w.h1.r)/2.0 -w.o.r, w.h1.r )
+        w.o.pdb_name = 'OW'
+        w.h1.pdb_name = 'HW1'
+        w.h2.pdb_name = 'HW2'
+        w.attach_properties( 
+                model = 'TIP3P_PDB',
+                method = 'B3LYP',
+                basis ='ANO631',
+                template_key = lambda x: x.pdb_name,
+                force = True
+                )
+        np.testing.assert_allclose( w.p.q , 0.0, atol = 1e-4 )
+
+    def test_attach_to_water(self):
+
+        w = Molecule.from_xyz_string( WATER )
+        p1, p2, p3 = w[0].r, (w[2].r - w[1].r)/2.0 -w[0].r, w[1].r 
+        t, r1, r2, r3 = utilz.center_and_xz( p1, p2, p3 )
+        w[0].pdb_name = 'OW'
+        w[1].pdb_name = 'HW1'
+        w[2].pdb_name = 'HW2'
+        w.attach_properties( 
+                model = 'TIP3P_PDB',
+                method = 'B3LYP',
+                basis ='ANO631',
+                template_key = lambda x: x.pdb_name,
+                force = True
+                )
+        np.testing.assert_allclose( w.p.q , 0.0, atol = 1e-4 )
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/test/test_euler.py
+++ src/test/test_euler.py
@@ -3,9 +3,9 @@ import numpy as np
 
 from use_generator import Generator
 from template import Template
-from molecules import Water, Atom, Property
+from molecules import Molecule, Water, Atom, Property
 
-class WaterTest( unittest.TestCase ):
+class WaterEulerTest( unittest.TestCase ):
 
     def setUp(self):
         self.ut_alpha = np.random.random( (6, ) )
@@ -18,6 +18,20 @@ class WaterTest( unittest.TestCase ):
         self.t1 = np.random.uniform( 0, np.pi/2 )
         self.t2 = np.random.uniform( 0, np.pi   )
         self.t3 = np.random.uniform( 0, np.pi/2 )
+
+    def test_euler(self):
+        a1 = Atom( pdb_name = 'ORIGO')
+        a2 = Atom( z = 1, pdb_name = 'Z')
+        a3 = Atom( x = 1, pdb_name = 'XZ')
+        m = Molecule( a1, a2, a3 )
+        m.t( 6, 6, 6 )
+        m.rotate( 0, 0, 0 )
+        f = lambda x: (x.get_atom_by_pdbname('ORIGO').r,
+                x.get_atom_by_pdbname('Z').r,
+                x.get_atom_by_pdbname('XZ').r)
+        r1, r2, r3 = m.get_euler( key = f )
+        np.testing.assert_allclose( [r1, r2, r3], 0.0, atol=1e-7 )
+
 
     def test_inv_rotation(self):
         """rotate two water molecules by random angles,
