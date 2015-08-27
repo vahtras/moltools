8e285c6ac8fe4db4af382a7726a03b4d06334a64
Ignat Harczuk <ignathe@gmail.com>
2015-08-27 19:12:31 +0200

    added test for populate bonds

--- src/test/test_bonds.py
+++ src/test/test_bonds.py
@@ -1,7 +1,7 @@
 import unittest, os
 import numpy as np
 
-from molecules import Cluster, Atom, Molecule
+from molecules import Cluster, Atom, Molecule, Water
 from use_generator import Generator
 
 FILE_MOL = os.path.join( os.path.dirname(__file__), 'tip3p44_10qm.mol' )
@@ -9,11 +9,13 @@ FILE_PDB = os.path.join( os.path.dirname(__file__), 'tip3p0.pdb' )
 
 class BondTestCase( unittest.TestCase ):
 
-    def setUp(self):
-        self.m = Molecule()
-
     def test_populate_bonds(self):
-        pass
+        w = Water.get_standard()
+        w.populate_bonds()
+        assert len( w.o.bonds ) == 2
+        assert len( w.h1.bonds ) == 1
+        assert len( w.h2.bonds ) == 1
+
 
 if __name__ == '__main__':
     unittest.main()
