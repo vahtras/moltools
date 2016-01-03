f9c64f47ecd665f65e0953dc90b4d5f89aa1d422
Ignat Harczuk <ignathe@gmail.com>
2016-01-03 14:32:33 +0100

    setup py doesnt import all molfiles for testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name="moltools",
     version="1.0",
-    packages=["src", "src/test", "src/loprop", ],
+    packages=["src/test", "src/loprop", "src/loprop/daltools", "src/loprop/daltools/util", "src/pd" ],
     scripts=["src/molecules.py", "src/pdbreader.py", ],
     author="Ignat Harczuk",
     author_email="harczuk@kth.se",
