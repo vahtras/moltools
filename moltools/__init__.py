2454fda07bc193538c44b5e3e565cdba28436fe8
Ignat Harczuk <ignathe@gmail.com>
2016-02-03 15:35:08 +0100

    readd old relative init file

---
+++ moltools/__init__.py
@@ -0,0 +1,26 @@
+#import everything
+
+from .molecules import *
+from .property import *
+from .pdbreader import *
+from .utilz import *
+from .plot_functions import *
+from .rotator import *
+from .template import *
+from .generator import *
+from .dstruct import *
+from .use_calculator import *
+
+__all__ = tuple(reduce( lambda a, x: a + x.__all__,  [molecules,
+        property,
+        pdbreader,
+        utilz,
+        plot_functions,
+        rotator,
+        template,
+        generator,
+        dstruct,
+        use_calculator,
+        ],
+        []))
+
