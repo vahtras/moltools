b4aec19030fd355b3c0720ce6e8e339543680ae5
Ignat Harczuk <ignathe@gmail.com>
2016-02-08 10:15:28 +0100

    update setup py to sdist upload to pypi

--- setup.py
+++ setup.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-
-
 setup_kwargs = {}
 
 setup_kwargs[ 'setup_requires' ] = [ 'numpy>=1.9.2', 'cython>=0.20.1' ]
 setup_kwargs[ 'install_requires' ] = [ 'numpy>=1.9.2',
-        'cython>=0.20.1',
         'matplotlib>=1.4.3',
         'loprop>=0.1',
-        'nose',
         'applequist>=0.1',
         ]
 try:
     import pkg_resources
-    from setuptools import setup, Command
+    from setuptools import setup, Command, find_packages
     _have_setuptools = True
 except:
     from distutils.core import setup, Command
     _have_setuptools = False
 
 setup(name="moltools",
-    version="1.0",
-    packages=[ "moltools", ],
+    version="1.1.1",
+    packages = find_packages(),
+    #package_dir = { '' : 'moltools' },
     scripts=[ "moltools/pdbreader.py", ],
     author="Ignat Harczuk",
     author_email="harczuk@kth.se",
     license = 'MIT',
     description = 'Running point dipole calculations on chemical systems',
     platforms = 'Linux',
+    maintainer = 'Ignat Harczuk',
     maintainer_email = 'ignathe@gmail.com',
     url = 'https://github.org/fishstamp82/moltools.git',
     **setup_kwargs
