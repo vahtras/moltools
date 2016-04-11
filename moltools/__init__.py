6832347b061043ace411bdfb7febf7d7746a1a29
Ignat Harczuk <ignathe@gmail.com>
2016-04-11 15:40:37 +0200

    udpate init ignore __all__

--- moltools/__init__.py
+++ moltools/__init__.py
@@ -11,16 +11,17 @@ from .generator import *
 from .dstruct import *
 from .use_calculator import *
 
-__all__ = tuple(reduce( lambda a, x: a + x.__all__,  [molecules,
-        property,
-        pdbreader,
-        utilz,
-        plot_functions,
-        rotator,
-        template,
-        generator,
-        dstruct,
-        use_calculator,
-        ],
-        []))
-
+#__all__ = tuple(reduce( lambda a, x: a + x.__all__, 
+#       [molecules,
+#        property,
+#        pdbreader,
+#        utilz,
+#        plot_functions,
+#        rotator,
+#        template,
+#        generator,
+#        dstruct,
+#        use_calculator,
+#        ],
+#        []))
+#
