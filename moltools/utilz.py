539af06db9da97dfeb520e0454938050b46d9bd6
Ignat Harczuk <ignathe@gmail.com>
2016-04-19 11:32:08 +0200

    added general rotation around x axis

--- moltools/utilz.py
+++ moltools/utilz.py
@@ -132,16 +132,33 @@ def Rz_inv( theta ):
                         [ -np.sin(theta), np.cos(theta), 0],
                         [ 0,             0,            1]])
     return vec
+
 def Ry( theta ):
     vec = np.array(    [[ np.cos(theta),0, np.sin(theta)],
                         [ 0,    1,  0],
                         [ -np.sin(theta), 0, np.cos(theta)]])
     return vec
+
 def Ry_inv( theta ):
     vec = np.array(    [[ np.cos(theta),0, -np.sin(theta)],
                         [ 0,    1,  0],
                         [ np.sin(theta), 0, np.cos(theta)]])
     return vec
+
+def Rx( theta ):
+    vec = np.array(    [[ 1, 0.0, 0.0 ],
+                        [ 0.0, np.cos(theta), -np.sin(theta)],
+                        [ 0.0, np.sin(theta), np.cos(theta)]] )
+    return vec
+
+def Rx( theta ):
+    vec = np.array(    [[ 1, 0.0, 0.0 ],
+                        [ 0.0, np.cos(theta), np.sin(theta)],
+                        [ 0.0, -np.sin(theta), np.cos(theta)]] )
+    return vec
+
+
+
 
 def R( axis, theta ):
     ux, uy, uz = axis
